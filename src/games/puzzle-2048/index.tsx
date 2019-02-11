7991544ed6935bcfaeebeab2b9a80e81b2811563
Oussama Zaki <oussama.zaki@klarna.com>
2019-02-11 23:55:08 +0100

    🎮 add 2048 puzzle game

---
+++ src/games/puzzle-2048/index.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+class Puzzle2048 extends React.Component {
+  render() {
+    return (
+      <div className="2048">
+        <div className="game-title">
+          <h2>2048</h2>
+          <h4>Number arrangement game.</h4>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Puzzle2048;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Route, Switch } from "react-router";
+
 import Home from "../home";
+import Puzzle2048 from "../games/puzzle-2048";
 import Concentration from "../games/concentration";
 
 const routes = (
   <Switch>
     <Route exact path="/" component={Home} />
     <Route exact path="/concentration" component={Concentration} />
+    <Route exact path="/2048" component={Puzzle2048} />
     <Route render={() => <div>404 Not found</div>} />
   </Switch>
 );
