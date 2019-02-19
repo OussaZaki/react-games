d5dc024cbe4cd22ae59a21f8e31ec8791e259860
Oussama Zaki <oussama.zaki@klarna.com>
2019-02-19 23:54:06 +0100

    🎮 switching to Functional component

--- src/games/concentration/index.tsx
+++ src/games/concentration/index.tsx
@@ -1,50 +1,29 @@
 import * as React from "react";
-import { Card } from "./model";
+import { GameProps } from "./model";
 import { FlipCard } from "./components";
 
-type State = {
-  cards: Card[];
-  loading: boolean;
-};
-
-class Concentration extends React.Component<{}, State> {
-  readonly state: State = {
-    cards: [],
-    loading: true
-  };
-
-  componentDidMount() {
-    // TODO: implement getCards from service.
-    const cards: Card[] = [];
-    this.setState({
-      cards,
-      loading: false
-    });
-  }
-
-  // TODO: dupplicate and shuffle cards.
-  render() {
-    const { loading, cards } = this.state;
-    return (
-      <div className="Concentration">
-        <div className="game-title">
-          <h2>Concentration</h2>
-          <h4>Memory game with matching cards.</h4>
-        </div>
-        {loading ? (
-          <div>Loading...</div>
-        ) : (
-          cards.map(card => (
-            <FlipCard
-              onClick={() => console.log("click")}
-              key={card.id}
-              content={card.svg}
-            />
-          ))
-        )}
-      </div>
-    );
-  }
-}
+const Concentration: React.FC<GameProps> = ({
+  loading,
+  cards,
+  handleCardClick
+}) => (
+  <div className="Concentration">
+    <div className="game-title">
+      <h2>Concentration</h2>
+      <h4>Memory game with matching cards.</h4>
+    </div>
+    {loading ? (
+      <div>Loading...</div>
+    ) : (
+      cards.map(card => (
+        <FlipCard
+          onClick={() => handleCardClick(card.id)}
+          key={card.id}
+          content={card.svg}
+        />
+      ))
+    )}
+  </div>
+);
 
 export default Concentration;
--- src/games/concentration/model.ts
+++ src/games/concentration/model.ts
@@ -1,25 +1,32 @@
-import { StateHandler } from "recompose";
-
 export interface Card {
   id: string;
   svg: string;
   flipped: boolean;
   found: boolean;
   isDuplicate?: boolean;
-};
+}
 
 export enum GameDifficulty {
   beginner = 1,
   mediun,
   advanced
-};
+}
 
-export type GameState = {
+export interface GameState {
   loading: boolean;
   cards: Card[];
   startingTime: number;
   started: boolean;
   gameDifficulty: GameDifficulty;
+}
+
+export type Props = {
+  cards: Card[];
+  loading: boolean;
 };
 
-export type StateHandlerFn = StateHandler<GameState>;
+export type Handlers = {
+  handleCardClick: (card: string) => void;
+};
+
+export type GameProps = Props & Handlers;
