6ecfe299253dfc565983567edb273e110ba762fd
Oussama Zaki <oussama.zaki@klarna.com>
2019-02-24 13:59:55 +0100

    [concentration]: ✅ full test coverage for withGameFlow

--- src/games/concentration/logic/withGameFlow.spec.ts
+++ src/games/concentration/logic/withGameFlow.spec.ts
@@ -1,5 +1,6 @@
 import { onCardClick } from "./withGameFlow";
 
+jest.useFakeTimers(); // using setTimeout within onCardClick.
 const props = {
   started: false,
   cardOnHold: undefined,
@@ -7,44 +8,30 @@ const props = {
   toggleHoldCard: jest.fn(),
   toggleFlips: jest.fn(),
   cardsComparison: jest.fn()
-} as any;
+};
+
 
 describe("withGameFlow | ", () => {
   describe("onCardClick | ", () => {
-    it("should return undefined when the clicked card is already flipped", () => {
+    it("should startTimer when the game did not start yet", () => {
       // Arrange
       const card = {
         id: "1",
         svg: "apple",
-        flipped: true,
+        flipped: false,
         found: false
       };
 
       // Act
-      const newState = onCardClick(props)(card);
+      onCardClick(props as any)(card);
 
       // Assert
-      expect(newState).toBeUndefined();
-    });
-
-    it("should return undefined when the clicked card is already found", () => {
-      // Arrange
-      const card = {
-        id: "1",
-        svg: "apple",
-        flipped: true,
-        found: true
-      };
-
-      // Act
-      const newState = onCardClick(props)(card);
-
-      // Assert
-      expect(newState).toBeUndefined();
+      expect(props.startTimer).toBeCalled();
     });
 
     it("should not startTimer when the game is already starting", () => {
       // Arrange
+      props.startTimer.mockClear();
       props.started = true;
       const card = {
         id: "1",
@@ -54,15 +41,46 @@ describe("withGameFlow | ", () => {
       };
 
       // Act
-      onCardClick(props)(card);
+      onCardClick(props as any)(card);
 
       // Assert
       expect(props.startTimer).not.toBeCalled();
     });
 
-    it("should startTimer when the game did not start yet", () => {
+    it("should return undefined when the clicked card is already found", () => {
       // Arrange
-      props.started = false;
+      const card = {
+        id: "1",
+        svg: "apple",
+        found: true,
+        flipped: true
+      };
+
+      // Act
+      const newState = onCardClick(props as any)(card);
+
+      // Assert
+      expect(newState).toBeUndefined();
+    });
+
+    it("should return undefined when the clicked card is already flipped", () => {
+      // Arrange
+      const card = {
+        id: "1",
+        svg: "apple",
+        found: false,
+        flipped: true
+      };
+
+      // Act
+      const newState = onCardClick(props as any)(card);
+
+      // Assert
+      expect(newState).toBeUndefined();
+    });
+
+    it("should flip the card when the clicked card is neither found not flipped", () => {
+      // Arrange
       const card = {
         id: "1",
         svg: "apple",
@@ -71,10 +89,10 @@ describe("withGameFlow | ", () => {
       };
 
       // Act
-      onCardClick(props)(card);
+      onCardClick(props as any)(card);
 
       // Assert
-      expect(props.startTimer).toBeCalled();
+      expect(props.toggleFlips).toBeCalledWith(card.id);
     });
 
     it("should put the first card onHold for comparison", () => {
@@ -88,12 +106,33 @@ describe("withGameFlow | ", () => {
       };
 
       // Act
-      onCardClick(props)(card);
+      const newState = onCardClick(props as any)(card);
 
       // Assert
       expect(props.toggleHoldCard).toBeCalled();
-      // TODO: this is redendant in this step since we have to flip the card anyway
-      expect(props.toggleFlips).toBeCalled();
+      expect(newState).toBeUndefined();
+    });
+
+    it("should compare cards if there is a card onHold", () => {
+      // Arrange
+      props.toggleHoldCard.mockClear();
+      props.cardOnHold = "1_1";
+      const card = {
+        id: "1",
+        svg: "apple",
+        flipped: false,
+        found: false
+      };
+
+      // Act
+      onCardClick(props as any)(card);
+
+      // Assert
+      expect(props.toggleHoldCard).not.toBeCalled();
+      expect(setTimeout).toBeCalled();
+
+      jest.runAllTimers();
+      expect(props.cardsComparison).toBeCalledWith(props.cardOnHold, card.id);
     });
   });
 });
