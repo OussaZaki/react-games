15b8560649dc61930c1cd7685e272adb4306549a
Oussama Zaki <oussama.zaki@klarna.com>
2019-02-24 13:58:59 +0100

    [concentration]: 🎨 re-arrange the gameflow in a simpler way

--- src/games/concentration/logic/withGameFlow.ts
+++ src/games/concentration/logic/withGameFlow.ts
@@ -2,23 +2,23 @@ import { withHandlers } from "recompose";
 
 import { GameProps, Card } from "../model";
 
-export const onCardClick = (props: Partial<GameProps>) => (card: Card) => {
-  if (card.found || card.flipped) {
-    return undefined;
-  }
-
+export const onCardClick = (props: GameProps) => (card: Card) => {
   if (!props.started) {
     props.startTimer();
   }
 
+  if (card.found || card.flipped) {
+    return;
+  }
+  props.toggleFlips(card.id);
+
   // Handle the first flipped card.
   if (!props.cardOnHold) {
     props.toggleHoldCard(card.id);
-    props.toggleFlips(card.id);
     return;
   }
 
-  props.toggleFlips(card.id);
+  // Handle second card flip and Delay comparison for a good UX
   setTimeout(() => {
     props.cardsComparison(props.cardOnHold!, card.id);
   }, 1000);
