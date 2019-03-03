6930ffc759a559bc8cfd2b78fe01c5bbe3d776c8
Oussama Zaki <oussama.zaki@klarna.com>
2019-03-03 23:59:04 +0100

    [2048]: ✨ add grid-row component

---
+++ src/games/puzzle-2048/components/grid-row/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+const GridRow: React.FC = () => (
+  <div className="grid-row">
+    <h1>GridRow goes here</h1>
+  </div>
+);
+
+export default GridRow;
--- src/games/puzzle-2048/components/index.ts
+++ src/games/puzzle-2048/components/index.ts
@@ -1,3 +1,5 @@
 export { default as Header } from "./header/index";
 export { default as Grid } from "./grid/index";
 export { default as GridCell } from "./grid-cell/index";
+export { default as GridRow } from "./grid-row/index";
+
