6cfcce7686a79217314c0f40ba0329803d5234f7
Oussama Zaki <oussama.zaki@klarna.com>
2019-03-04 03:44:11 +0100

    [concentration]: 🎨 cleanup concentration styles and proper load svgs

--- src/games/concentration/components/flip-card/index.tsx
+++ src/games/concentration/components/flip-card/index.tsx
@@ -1,23 +1,20 @@
-
 import * as React from "react";
 import { compose } from "recompose";
+import LazyLoad from "react-lazyload";
+
 import { FlipCardProps, Props, withStyleProps } from "./behavior";
 
 import "./styles.scss";
 
-const FlipCard: React.FC<FlipCardProps> = ({
-  onClick,
-  content,
-  className
-}) => (
+const FlipCard: React.FC<FlipCardProps> = ({ onClick, content, className }) => (
   <div className={className} onClick={onClick}>
-    <div className="front">
-      <div className="child">{content}</div>
+    <div className="flipcard-inner front">
+      <LazyLoad height={200}>
+        <img src={content} />
+      </LazyLoad>
     </div>
-    <div className="back" />
+    <div className="flipcard-inner back" />
   </div>
 );
 
-export default compose<FlipCardProps, Props>(
-  withStyleProps
-)(FlipCard);
+export default compose<FlipCardProps, Props>(withStyleProps)(FlipCard);
--- src/games/concentration/index.tsx
+++ src/games/concentration/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { compose } from "recompose";
 
-import { GameProps } from "./model";
+import { GameProps, Card } from "./model";
 import { FlipCard } from "./components";
 import {
   withGameStateHandlers,
@@ -10,6 +10,17 @@ import {
   withLifecycle
 } from "./logic";
 
+const _getFlipCards = (cards: Card[], clickHandler: (card: Card) => void) =>
+  cards.map(card => (
+    <FlipCard
+      onClick={() => clickHandler(card)}
+      key={card.id}
+      content={card.svg}
+      flipped={card.flipped}
+      found={card.found}
+    />
+  ));
+
 const Concentration: React.FC<GameProps> = ({
   loading,
   gameWon,
@@ -17,24 +28,12 @@ const Concentration: React.FC<GameProps> = ({
   onCardClick
 }) => (
   <div className="Concentration">
-    <div className="game-title">
+    <div className="header">
       <h2>Concentration</h2>
       <h4>Memory game with matching cards.</h4>
+      {gameWon && <h3>Congrats!</h3>}
     </div>
-    {gameWon && <div>Congrats!</div>}
-    {loading ? (
-      <div>Loading...</div>
-    ) : (
-      cards.map(card => (
-        <FlipCard
-          onClick={() => onCardClick(card)}
-          key={card.id}
-          content={card.svg}
-          flipped={card.flipped}
-          found={card.found}
-        />
-      ))
-    )}
+    {loading ? <div>Loading...</div> : _getFlipCards(cards, onCardClick)}
   </div>
 );
 
